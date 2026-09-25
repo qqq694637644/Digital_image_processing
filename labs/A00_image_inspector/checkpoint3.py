48b293e141180efe069c538ae1fc8779495b5208
qqq2689988983 <qqq2689988983@gmail.com>
2026-09-25 11:49:52 +0800

    111

---
+++ labs/A00_image_inspector/checkpoint3.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import sys
+import cv2
+import matplotlib.pyplot as plt
+def main():
+    if len(sys.argv)!=2:
+        print("usage: python main.py image.jpg")
+        raise SystemExit(1)
+    image_path =Path(sys.argv[1])
+    image = cv2.imread(str(image_path))
+    if image is None:
+        raise ValueError(f"failed to read image:{image_path}")
+    # print("shape:",image.shape)
+    # print("dtype:",image.dtype)
+    # print("pixel[0,0]:",image[0,0])
+    print("min:",image.min())
+    print("max:",image.max())
+    print("mean:",image.mean())
+    print("std:",image.std())
+    bgr = image
+    rgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor((image,cv2.COLOR_BGR2GRAY))
+if __name__ =="__main__":
+    main()
